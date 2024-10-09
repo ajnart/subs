09bdf5a5881df8ec671741bdbc0149fc814515e0
ajnart <ajnart@pm.me>
2024-10-10 00:54:46 +0900

    Update db schema (kv store)

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,24 +1,19 @@
 import { sql } from "drizzle-orm";
 import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
 
-export const createTable = sqliteTableCreator((name) => `shawty_${name}`);
+export const createTable = sqliteTableCreator((name) => `subs_${name}`);
 
-export const subscriptions = createTable(
-	"subscription",
+export const kvStore = createTable(
+	"kv_store",
 	{
-		id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-		name: text("name", { length: 256 }).notNull(),
-		url: text("url", { length: 2048 }).notNull(),
-		price: int("price").notNull(),
-		icon: text("icon", { length: 2048 }).notNull(),
-		createdAt: int("created_at", { mode: "timestamp" })
+		key: text("key").primaryKey(),
+		value: text("value").notNull(),
+		updatedAt: int("updated_at", { mode: "timestamp" })
 			.default(sql`(unixepoch())`)
-			.notNull(),
-		updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
-			() => new Date(),
-		),
+			.notNull()
+			.$onUpdate(() => new Date()),
 	},
-	(subscription) => ({
-		nameIndex: index("name_idx").on(subscription.name),
+	(table) => ({
+		keyIndex: index("key_idx").on(table.key),
 	}),
 );
